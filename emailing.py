8a6f5dc4f58debdba8ac27eb229e431f10ca2c92
Qde <nzemka0@gmail.com>
2023-09-27 19:13:41 +0300

    add email send function, realized clean images folder function, add threading to fix camera lags

--- emailing.py
+++ emailing.py
@@ -1,2 +1,32 @@
-def send_email():
-    print("Email was sent!")
+import smtplib
+import os
+from email.message import EmailMessage
+import imghdr
+
+password = os.getenv("PASSWORD")
+sender = "zemka.kit@gmail.com"
+receiver = "zemka.kit@gmail.com"
+
+
+def send_email(image_path):
+    print("Email send started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object detected!"
+    email_message.set_content("Hey, we just saw a new object!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender, password)
+    gmail.sendmail(sender, receiver, email_message.as_string())
+    gmail.quit()
+    print("email send ended")
+
+if __name__ == "__main__":
+    send_email(image_path="images/15.png")
+
+
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import glob
 import cv2
 import time
+import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    print("Clean started")
+    images = glob.glob("images/*png")
+    for i in images:
+        os.remove(i)
+    print("Clean ended")
+
+
 while True:
     status = 0
     check, frame = video.read()
@@ -18,26 +32,36 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
-    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 5000:
+        if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+
+        email_thread.start()
+
 
     print(status_list)
 
@@ -47,4 +71,7 @@ while True:
     if key == ord("q"):
         break
 
+clean_thread = Thread(target=clean_folder)
+clean_thread.daemon = True
+clean_thread.start()
 video.release()
